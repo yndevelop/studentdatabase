6f12717e59405fafefdfd995794519a1b8335394
yndevelop <80019861+yndevelop@users.noreply.github.com>
2021-06-10 23:52:39 -0700

    Add files via upload

---
+++ studentdatabse.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 11 23:04:54 2021
+
+@author: user
+"""
+
+#Dictionaries
+#students = {"Yahunathan":26,"Sarah":28}
+#students = {"Yahunathan":26,"Sarah":28}
+#create a list and dict.
+
+students = {
+        "Yahunathan":{"id":"ID0001","age":33,"grade":"C"}, 
+        "Aaron":{"id":"ID0031","age":18,"grade":"A"},
+        "Caleb":{"id":"ID3301","age":45,"grade":"C"},
+        "Cody":{"id":"ID0941","age":45,"grade":"B"}, 
+        "Megan":{"id":"ID0781","age":12,"grade":"B"}, 
+        "Sarah":{"id":"ID0021","age":24,"grade":"D"}
+       } 
+
+print(students["Yahunathan"]["age"])
+            
